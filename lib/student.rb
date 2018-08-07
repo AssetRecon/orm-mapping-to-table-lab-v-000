32171ba3add3058d69e3c1ead2590761ea679df4
Edsel Caprice <caprice@mail.usf.edu>
2018-08-07 00:47:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,13 @@ class Student
   def self.create_table
      sql = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)"
 
-
+#     sql =  <<-SQL
+#   CREATE TABLE IF NOT EXISTS students (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT,
+#     grade TEXT
+#     )
+#     SQL
 
      DB[:conn].execute(sql)
   end
